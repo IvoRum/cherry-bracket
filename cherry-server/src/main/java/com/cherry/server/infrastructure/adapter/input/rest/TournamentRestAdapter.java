4005eca891213fbfa59a2d45154541cac9c58a1c
IvoRum <ivailo.rumenov@outlook.com>
2025-05-22 22:39:19 +0300

    Created rest endpoint for creating public tournament

--- cherry-server/src/main/java/com/cherry/server/infrastructure/adapter/input/rest/TournamentRestAdapter.java
+++ cherry-server/src/main/java/com/cherry/server/infrastructure/adapter/input/rest/TournamentRestAdapter.java
@@ -1,12 +1,24 @@
 package com.cherry.server.infrastructure.adapter.input.rest;
 
 import com.cherry.server.application.ports.input.CreateTournamentCase;
+import com.cherry.server.domain.model.Tournament;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/tournament")
 @RequiredArgsConstructor
-public class TournamentRestAdapter implements CreateTournamentCase {
+public class TournamentRestAdapter{
+    private final CreateTournamentCase createTournamentCase;
+
+    @PostMapping("/create/public")
+    ResponseEntity<Tournament> save(@RequestBody Tournament tournament) {
+        createTournamentCase.createPublic(tournament);
+        return new ResponseEntity<>(tournament, HttpStatus.CREATED);
+    }
 }
